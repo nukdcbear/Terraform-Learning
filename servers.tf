17cad53ac3c82919d3a63b3ac707c09faaf1ad45
DCBarringer <davidcbarringer@gmail.com>
2019-06-04 13:34:35 -0400

    Creatng subnets and a gcp compute resource

---
+++ servers.tf
@@ -0,0 +1,28 @@
+resource "google_compute_instance" "firstserver" {
+  name = "thefirstserver"
+  machine_type = "n1-standard-1"
+  zone = "us-central1-a"
+
+  boot_disk {
+      initialize_params {
+          image = "debian-cloud/debian-9"
+      }
+  }
+
+  network_interface {
+      subnetwork = "${google_compute_subnetwork.dev-subnet.name}"
+
+      access_config {
+
+      }
+  }
+
+    metadata = {
+        foo = "bar"
+    }
+
+    service_account {
+        scopes = ["userinfo-email", "compute-ro", "storage-ro"]
+    }
+
+}
